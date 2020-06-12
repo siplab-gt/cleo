27bee27f444bff381772ba1a498c430ec9977c40
kjohnsen <kjohnsen6@gatech.edu>
2020-06-12 15:17:26 -0400

    Implement Sim-CLoop communication

--- clocsim/base.py
+++ clocsim/base.py
@@ -1,4 +1,4 @@
-from brian2 import NeuronGroup, Network
+from brian2 import NeuronGroup, Network, ms, NetworkOperation
 from abc import ABC, abstractmethod
 
 class InterfaceDevice(ABC):
@@ -19,7 +19,7 @@ class ControlLoop(ABC):
 
     # The output should be a dictionary of {stimulator_name: value, ...}
     @abstractmethod
-    def get_control_signal(self, time) -> dict:
+    def get_ctrl_signal(self, time) -> dict:
         pass
 
 
@@ -27,9 +27,17 @@ class Recorder(InterfaceDevice):
     def __init__(self):
         pass
 
+    @abstractmethod
+    def get_state(self):
+        pass
+
 
 class Stimulator(InterfaceDevice):
     def __init__(self):
+        pass
+
+    @abstractmethod
+    def update(self, ctrl_signal):
         pass
 
 
@@ -54,9 +62,29 @@ class CLOCSimulator:
         for brian_object in recorder.brian_objects:
             self.network.add(brian_object)
 
-    def set_control_loop(self, control_loop, control_period):
-        self.controller = controller
-        self.network.
+    def get_state(self):
+        state = {}
+        for name, recorder in self.recorders.items():
+            state[name] = recorder.get_state()
+        return state
+
+    def update_controllers(self, ctrl_signal):
+        for name, signal in ctrl_signal.items():
+            self.stimulators[name].update(signal)
+
+    def set_control_loop(self, control_loop, sample_period=1*ms, poll_ctrl_period=0.1*ms):
+        self.control_loop = control_loop
+
+        def send_signal(t):
+            control_loop.put_state(self.get_state(), t)
+
+        def receive_signal(t):
+            ctrl_signal = control_loop.get_ctrl_signal(t)
+            self.update_controllers(ctrl_signal)
+
+        self.network.add(NetworkOperation(send_signal, dt=sample_period))
+        self.network.add(NetworkOperation(receive_signal, dt=poll_ctrl_period))
+
 
 
     def run(self, duration):
