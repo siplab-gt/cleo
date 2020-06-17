fc2a5f0e54f8f37708bbb0d41d52ebbdb2f272b8
kjohnsen <kjohnsen6@gatech.edu>
2020-06-17 13:33:37 -0400

    Create SpikeRecorder

--- clocsim/recorders.py
+++ clocsim/recorders.py
@@ -1,4 +1,4 @@
-from brian2 import PopulationRateMonitor, StateMonitor
+from brian2 import PopulationRateMonitor, StateMonitor, SpikeMonitor
 from .base import Recorder
 
 
@@ -33,3 +33,23 @@ class VoltageRecorder(Recorder):
             return self.mon.v[:, -1]
         except IndexError:
             return None
+
+class SpikeRecorder(Recorder):
+    '''
+    Reports the number of spikes seen since last queried, so effectively
+    the number of spikes per control period.
+    '''
+    def __init__(self, name, index):
+        super().__init__(name)
+        self.i = index
+        self.mon = None
+        self.num_spikes_seen = 0
+
+    def connect_to_neurons(self, neuron_group):
+        self.mon = SpikeMonitor(neuron_group, record=self.i)
+        self.brian_objects.add(self.mon)
+    
+    def get_state(self):
+        num_new_spikes = len(self.mon.t) - self.num_spikes_seen
+        self.num_spikes_seen += num_new_spikes
+        return num_new_spikes
