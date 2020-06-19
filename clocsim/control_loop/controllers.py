9f098e434cf840e36d606058e03fde75b53a64b4
kjohnsen <kjohnsen6@gatech.edu>
2020-06-19 15:31:28 -0400

    Implement PI Controller

--- clocsim/control_loop/controllers.py
+++ clocsim/control_loop/controllers.py
@@ -1,5 +1,24 @@
 from . import LoopComponent
+from typing import Any
+from nptyping import NDArray
 
 class Controller(LoopComponent):
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+
+class PIController(Controller):
+    def __init__(self, ref_signal, Kp, Ki=0, sample_period_ms=0, **kwargs):
+        '''
+        ref_signal is a function of time
+        '''
+        super().__init__(kwargs)
+        self.ref_signal = ref_signal
+        self.Kp = Kp
+        self.Ki = Ki
+        self.sample_period = sample_period_ms
+        self.integrated_error = 0
+    
+    def _process_data(self, data, time: float):
+        error = self.ref_signal(time) - data
+        self.integrated_error += error*self.sample_period
+        return self.Kp*error + self.Ki * self.integrated_error
