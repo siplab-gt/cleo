ccdeae5da276166d99f876b2516e46efdb781403
kjohnsen <kjohnsen6@gatech.edu>
2020-06-22 13:19:22 -0400

    Replace CurrentPulseStim with StateVariableSetter

--- clocsim/stimulators.py
+++ clocsim/stimulators.py
@@ -1,14 +1,16 @@
 from .base import Stimulator
 from brian2 import NeuronGroup
 
-class CurrentPulseStimulator(Stimulator):
-    def __init__(self, name, index):
+class StateVariableSetter(Stimulator):
+    def __init__(self, name, index, variable_to_ctrl, unit):
         super().__init__(name)
         self.i = index
-        self.neuron_group = None
+        self.neurons = None
+        self.var = variable_to_ctrl
+        self.unit = unit
 
     def connect_to_neurons(self, neuron_group):
         self.neurons = neuron_group[self.i]
 
     def update(self, ctrl_signal):
-        self.neurons.I = ctrl_signal
+        setattr(self.neurons, self.var, ctrl_signal*self.unit)
