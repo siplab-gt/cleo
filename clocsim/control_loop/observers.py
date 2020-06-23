Fix time unit conversion concerns

clocsim/control_loop/observers.py
@@ -10,12 +10,12 @@ class FiringRateEstimator(LoopComponent):
         See `LoopComponent` for `**kwargs` options
         '''
         super().__init__(**kwargs)
-        self.tau = tau_ms
-        self.T = sample_period_ms
+        self.tau_s = tau_ms / 1000
+        self.T_s = sample_period_ms / 1000
         self.alpha = np.exp(-sample_period_ms / tau_ms)
         self.prev_rate = None
 
-    def _process_data(self, data: NDArray[(1, Any), Int32]) \
+    def _process_data(self, data: NDArray[(1, Any), Int32], time_ms=None) \
             -> NDArray[(1, Any), float]:
         '''
         `data` should be a vector of spike counts.
@@ -23,6 +23,6 @@ class FiringRateEstimator(LoopComponent):
         if self.prev_rate is None:
             self.prev_rate = np.zeros(data.shape)
 
-        curr_rate = self.prev_rate*self.alpha + (1-self.alpha)*data/self.T
+        curr_rate = self.prev_rate*self.alpha + (1-self.alpha)*data/self.T_s
         self.prev_rate = curr_rate
         return curr_rate