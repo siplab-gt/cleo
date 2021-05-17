bec852057e220d8556b2c1eaa4aa4b2022606beb
kjohnsen <kjohnsen6@gatech.edu>
2021-05-17 18:03:40 -0400

    Start catching up on linting/docs

--- clocsim/control_loop/__init__.py
+++ clocsim/control_loop/__init__.py
@@ -1,3 +1,5 @@
+"""Classes and functions for constructing and configuring a :func:`~base.ControlLoop`."""
+
 from abc import ABC, abstractmethod
 from typing import Tuple, Any
 from collections import deque
@@ -7,33 +9,76 @@ from brian2 import ms
 from ..base import ControlLoop
 from .delays import Delay
 
+
 class LoopComponent(ABC):
+    """Abstract signal processing stage or control block."""
+
     def __init__(self, **kwargs):
+        """Construct a `LoopComponent` object.
+
+        It's important to use `super().__init__(kwargs)` in the base class
+        to use the parent-class logic here.
+
+        Raises
+        ------
+        TypeError
+            When `delay` is not a `Delay` object.
+        """
         self.delay = kwargs.get('delay', None)
+        if type(self.delay) != Delay:
+            raise TypeError('delay must be of the Delay class')
         self.save_history = kwargs.get('save_history', False)
         if self.save_history is True:
             self.t = []
             self.out_t = []
             self.values = []
 
-    def process_data(self, data, time_ms: float) -> Tuple[Any, float]:
-        out = self._process_data(data, time_ms)
+    def process(self, input: Any, in_time_ms: float) -> Tuple[Any, float]:
+        """Compute output and output time given input and input time.
+
+        The user should implement :func:`~_process`, which performs the 
+        computation itself without regards for the delay.
+
+        Parameters
+        ----------
+        input : Any
+        in_time_ms : float
+
+        Returns
+        -------
+        Tuple[Any, float]
+            output, out time
+        """
+        out = self._process(input, in_time_ms)
         if self.delay is not None:
-            out_time_ms = self.delay.add_delay_to_time(time_ms)
+            out_time_ms = self.delay.add_delay_to_time(in_time_ms)
         else:
-            out_time_ms = time_ms
+            out_time_ms = in_time_ms
         if self.save_history:
-            self.t.append(time_ms)
+            self.t.append(in_time_ms)
             self.out_t.append(out_time_ms)
             self.values.append(out)
         return (out, out_time_ms)
 
     @abstractmethod
-    def _process_data(self, data, time_ms: float = None) -> Any:
-        '''
-        This is the method that must be implemented, which will process
-        the data without needing to account for time delay.
-        '''
+    def _process(self, input: Any, in_time_ms: float = None) -> Any:
+        """Computes output for given input and input time.
+
+        This is where the user will implement the desired functionality
+        of the `LoopComponent` without regard for latency.
+
+        Parameters
+        ----------
+        input : Any
+        in_time_ms : float, optional
+            For use in time-dependent computations, e.g., tracking a time-varying
+            reference trajectory. By default None.
+
+        Returns
+        -------
+        Any
+            output.
+        """
         pass
 
 
@@ -95,15 +140,28 @@ class DelayControlLoop(ControlLoop):
                 else:  # if not done computing
                     self.overrun = True
                     return False
-            else:  # off-schedule, only sample if the last sampling period was missed (there was an overrun)
+            else:
+                # off-schedule, only sample if the last sampling period
+                # was missed (there was an overrun)
                 return self.overrun
         return False
 
     @abstractmethod
     def compute_ctrl_signal(self, state_dict: dict, time_ms: float) -> Tuple[dict, float]:
-        ''' 
-        Must return a tuple (dict, delayed_time). This function is where you set
-        up the data processing pipeline. The output dictionary must have one name-value
-        pair for each stimulator you want to control.
-        '''
+        """Process network state to generate output to update stimulators.
+
+        This is the function the user must implement to define the signal processing
+        pipeline.
+
+        Parameters
+        ----------
+        state_dict : dict
+            {`recorder_name`: `state`} dictionary from :func:`~base.CLOCSimulator.get_state()`
+        time_ms : float
+
+        Returns
+        -------
+        Tuple[dict, float]
+            {'stim_name`: `ctrl_signal`} dictionary and output time in milliseconds.
+        """
         pass
