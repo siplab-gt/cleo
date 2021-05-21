0dabebe100ade2dfa1276a8626c1805e7226804e
kjohnsen <kjohnsen6@gatech.edu>
2021-05-21 16:33:05 -0400

    Finish test_recorders

--- tests/recorders/test_recorders.py
+++ tests/recorders/test_recorders.py
@@ -1,13 +1,18 @@
 import pytest
 from brian2 import NeuronGroup, Network, ms
 
-from clocsim.recorders import RateRecorder, VoltageRecorder, GroundTruthSpikeRecorder
+from clocsim.recorders import (
+    RateRecorder,
+    VoltageRecorder,
+    GroundTruthSpikeRecorder,
+)
 from clocsim import CLOCSimulator
 
 
 @pytest.fixture
 def neurons():
-    return NeuronGroup(1, "v = -70 : 1", threshold="v > -50")
+    # will spike every time
+    return NeuronGroup(1, "v = -70 : 1", threshold="v > -80")
 
 
 @pytest.fixture
@@ -16,12 +21,30 @@ def sim(neurons):
     return CLOCSimulator(net)
 
 
-def test_RateRecorder(sim, neurons, mocker):
+def test_RateRecorder(sim, neurons):
     rate_rec = RateRecorder("rate_rec", 0)
-    sim.inject_recorder(sim, neurons)
+    sim.inject_recorder(rate_rec, neurons)
     assert rate_rec.get_state() == 0
 
-    sim.run(1 * ms)
+    sim.run(0.3 * ms)  # will spike at every 0.1 ms timestep
 
-    mocker.patch("rate_rec.mon.rate", return_value=[1])
-    assert rate_rec.get_state() == 1
+    assert rate_rec.get_state() == 10 / ms
+
+
+def test_VoltageRecorder(sim, neurons):
+    v_rec = VoltageRecorder("v_rec", 0)
+    sim.inject_recorder(v_rec, neurons)
+
+    sim.run(0.1 * ms)  # will spike at every 0.1 ms timestep
+    assert v_rec.get_state() == -70
+
+
+def test_GroundTruthSpikeRecorder(sim, neurons):
+    spike_rec = GroundTruthSpikeRecorder("spike_rec", 0)
+    sim.inject_recorder(spike_rec, neurons)
+
+    sim.run(0.1 * ms)  # will spike at every 0.1 ms timestep
+    assert spike_rec.get_state() == 1
+
+    sim.run(0.2 * ms)
+    assert spike_rec.get_state() == 2
