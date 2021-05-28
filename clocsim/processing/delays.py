3c6f47b1cc86cadd3ae57b8389299a6b18b3d531
kjohnsen <kjohnsen6@gatech.edu>
2021-05-28 16:56:51 -0400

    Finish basic delays

--- clocsim/processing/delays.py
+++ clocsim/processing/delays.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
+import warnings
 
-'''Delays should all assume the unit of milliseconds.
-'''
+import numpy as np
+
+"""Delays should all assume the unit of milliseconds.
+"""
+
+
 class Delay(ABC):
     @abstractmethod
-    def add_delay_to_time(self, time) -> float:
+    def compute(self) -> float:
         pass
 
 
@@ -12,5 +17,28 @@ class ConstantDelay(Delay):
     def __init__(self, delay_ms):
         self.delay = delay_ms
 
-    def add_delay_to_time(self, time):
-        return time + self.delay
+    def compute(self):
+        return self.delay
+
+
+class GaussianDelay(Delay):
+    """Generates normal-distributed delay.
+
+    Will return 0 when a negative value is sampled.
+
+    Parameters
+    ----------
+    Delay : [type]
+        [description]
+    """
+
+    def __init__(self, loc, scale):
+        self.loc = loc
+        self.scale = scale
+
+    def compute(self) -> float:
+        out = np.random.normal(self.loc, self.scale)
+        if out < 0:
+            warnings.warn("Negative delay sampled. Returning 0.")
+            out = 0
+        return out
--- tests/processing/test_delays.py
+++ tests/processing/test_delays.py
@@ -1,5 +1,21 @@
 """Test basic Delay classes in processing.delays.py"""
 
+import numpy as np
+import pytest
 
-def test_ConstDelay():
-    pass
+from clocsim.processing.delays import *
+
+
+def test_ConstantDelay():
+    assert ConstantDelay(1).compute() == 1
+    assert ConstantDelay(5).compute() == 5
+
+
+def test_GaussianDelay():
+    np.random.seed(1820)
+    assert GaussianDelay(1, 0.1).compute() == 0.9398680594942359
+    assert GaussianDelay(1, 0.1).compute() == 0.9611997124217544
+    with pytest.warns(UserWarning):
+        assert GaussianDelay(-1, 0.01).compute() == 0
+    assert GaussianDelay(5, 1).compute() == 3.9169426457170893
+    assert GaussianDelay(5, 1).compute() == 4.636290455175261
