88026e1c8fe4714b41034b7d2f96d1c27e197fe9
Kyle Johnsen <johnsenkyle13@gmail.com>
2021-11-04 18:01:17 -0400

    Get simple_pi_example working

--- clocsim/processing/controllers.py
+++ clocsim/processing/controllers.py
@@ -7,6 +7,7 @@ class Controller(LoopComponent):
         super().__init__(**kwargs)
 
 class PIController(Controller):
+    """`process()` requires `sample_time_ms` kwarg"""
     ref_signal: Callable[[float], Any]
 
     def __init__(self, ref_signal, Kp, Ki=0, sample_period_ms=0, **kwargs):
@@ -21,11 +22,12 @@ class PIController(Controller):
         self.integrated_error = 0
         self.prev_time_ms = None
     
-    def _process_data(self, data, time_ms: float):
+    def _process(self, input, **kwargs):
+        time_ms = kwargs['sample_time_ms']
         if self.prev_time_ms is None:
             self.prev_time_ms = time_ms - self.sample_period_s*1000
         intersample_period_s = (time_ms - self.prev_time_ms)/1000
-        error = self.ref_signal(time_ms) - data
+        error = self.ref_signal(time_ms) - input
         self.integrated_error += error*intersample_period_s
         self.prev_time_ms = time_ms
         return self.Kp*error + self.Ki * self.integrated_error
--- clocsim/processing/observers.py
+++ clocsim/processing/observers.py
@@ -5,6 +5,15 @@ from typing import Any
 from .delays import Delay
 
 class FiringRateEstimator(LoopComponent):
+    """Exponential filter to estimate firing rate.
+
+    Requires `sample_time_ms` kwarg when process is called.
+
+    Parameters
+    ----------
+    LoopComponent : [type]
+        [description]
+    """
     def __init__(self, tau_ms: float, sample_period_ms: float, **kwargs):
         '''
         See `LoopComponent` for `**kwargs` options
@@ -16,19 +25,21 @@ class FiringRateEstimator(LoopComponent):
         self.prev_rate = None
         self.prev_time_ms = None
 
-    def _process_data(self, data: NDArray[(1, Any), Int32], time_ms=None) \
+    # def _process(self, data: NDArray[(1, Any), Int32], time_ms=None) \
+    def _process(self, input: NDArray[(1, Any), Int32], **kwargs) \
             -> NDArray[(1, Any), float]:
         '''
-        `data` should be a vector of spike counts.
+        `input` should be a vector of spike counts.
         '''
+        time_ms = kwargs['sample_time_ms']
         if self.prev_rate is None:
-            self.prev_rate = np.zeros(data.shape)
+            self.prev_rate = np.zeros(input.shape)
         if self.prev_time_ms is None:
             self.prev_time_ms = time_ms - self.T_s*1000
 
         intersample_period_s = (time_ms - self.prev_time_ms)/1000
         alpha = np.exp(-intersample_period_s / self.tau_s)
-        curr_rate = self.prev_rate*alpha + (1-alpha)*data/intersample_period_s
+        curr_rate = self.prev_rate*alpha + (1-alpha)*input/intersample_period_s
         self.prev_rate = curr_rate
         self.prev_time_ms = time_ms
         return curr_rate
