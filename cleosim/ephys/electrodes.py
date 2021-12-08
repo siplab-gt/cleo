4ccaf630a11a1c7db1742355b48559d000425b04
Kyle Johnsen <johnsenkyle13@gmail.com>
2021-12-08 11:17:36 -0500

    Finish base electrode group

--- cleosim/ephys/electrodes.py
+++ cleosim/ephys/electrodes.py
@@ -1,14 +1,69 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from collections.abc import Iterable
+from typing import Any, Tuple
+
+import numpy as np
+from numpy.core import numeric
+import numpy.typing as npt
+from brian2 import NeuronGroup, mm, Unit
+
 from cleosim import Recorder
-# probe coords
 
-# array coords
 
-# signal
-class Signal:
-    pass
+class Signal(ABC):
+    name: str
+    brian_objects: set
 
-# electrode group
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.brian_objects = set()
+
+    @abstractmethod
+    def connect_to_neuron_group(self, neuron_group: NeuronGroup, **kwparams):
+        pass
+
+    @abstractmethod
+    def get_state(self) -> Any:
+        pass
+
+
 class ElectrodeGroup(Recorder):
-    def __init__(self, name, coords, signals=[]):
-        super().__init__(name)
+    coords: npt.NDArray
+    signals: Iterable[Signal]
+    n: int
 
+    def __init__(
+        self, name: str, coords: npt.ArrayLike, signals: Iterable[Signal] = []
+    ):
+        super().__init__(name)
+        self.coords = np.array(coords).reshape((-1, 3))
+        if len(self.coords.shape) != 2 or self.coords.shape[1] != 3:
+            raise ValueError(
+                "coords must be an n by 3 array with x, y, and z coordinates"
+                "for n contact locations."
+            )
+        self.n = len(self.coords)
+        self.signals = signals
+
+    def connect_to_neuron_group(self, neuron_group: NeuronGroup, **kwparams):
+        for signal in self.signals:
+            signal.connect_to_neuron_group(neuron_group, **kwparams)
+            self.brian_objects.update(signal.brian_objects)
+
+    def get_state(self):
+        state_dict = {}
+        for signal in self.signals:
+            state_dict[signal.name] = signal.get_state()
+        return state_dict
+
+
+def get_1D_probe_coords(
+    length: float * Unit,
+    channel_count: int,
+    base_location: Tuple[float, float, float] * Unit = (0, 0, 0) * mm,
+    direction: Tuple[float, float, float] * Unit = (0, 0, 1),
+) -> npt.NDArray:
+    dir_uvec = direction / np.linalg.norm(direction)
+    tip_location = base_location + length * dir_uvec
+    return np.linspace(base_location, tip_location, channel_count)
--- tests/recorders/test_electrodes.py
+++ tests/recorders/test_electrodes.py
@@ -1,15 +1,65 @@
 """Tests for electrodes module"""
-from brian2 import NeuronGroup
-from cleosim.ephys import ElectrodeGroup
-# from cleosim.recorders.electrodes import ElectrodeGroup
+from typing import Tuple, Any
+
+import numpy as np
+import pytest
+from brian2 import NeuronGroup, mm, Network, StateMonitor
+
+import cleosim
+from cleosim import CLSimulator
+from cleosim.ephys import ElectrodeGroup, get_1D_probe_coords, Signal
+
 
 def test_ElectrodeGroup():
     eg = ElectrodeGroup("eg", [0, 0, 0])
     assert eg.n == 1
+    eg = ElectrodeGroup("eg", [[0, 0, 0], [1, 1, 1]])
+    assert eg.n == 2
+    with pytest.raises(ValueError):
+        ElectrodeGroup("eg", [0, 0])
+    with pytest.raises(ValueError):
+        ElectrodeGroup("eg", [[0, 0], [1, 1]])
+
+
+def test_electrode_injection():
+    class DummySignal(Signal):
+        def connect_to_neuron_group(self, neuron_group: NeuronGroup, **kwparams):
+            self.ng = neuron_group
+            self.brian_objects.add(StateMonitor(ng, "v", record=True))
+
+        def get_state(self) -> Any:
+            return self.ng.name
+
+    ng = NeuronGroup(1, "v : 1")
+    sim = CLSimulator(Network(ng))
+
+    dumb = DummySignal("dumb")
+    dumber = DummySignal("dumber")
+    eg = ElectrodeGroup("eg", [0, 0, 0], signals=[dumb, dumber])
+    sim.inject_recorder(eg, ng)
+
+    assert dumb.brian_objects.issubset(sim.network.objects)
+    assert dumber.brian_objects.issubset(sim.network.objects)
+    assert sim.get_state()["eg"]["dumb"] == ng.name
+    assert sim.get_state()["eg"]["dumber"] == ng.name
+
 
 def test_probe_coords():
-    pass
+    coords = get_1D_probe_coords(
+        base_location=(0, 0, 0.1) * mm,
+        direction=(0, 0, 0.01),
+        length=1 * mm,
+        channel_count=32,
+    )
+    assert coords.shape == (32, 3)
+    # not using norm because it strips units
+    assert np.sqrt(np.sum((coords[0] - coords[-1]) ** 2)) == 1 * mm
 
-def test_array_coords():
-    pass
-
+    coords2 = get_1D_probe_coords(
+        base_location=(1, 1, 0.1) * mm,
+        direction=(0, 0, 0.01),
+        length=1 * mm,
+        channel_count=32,
+    )
+    # no matching rows between them
+    assert not np.any(np.all(coords == coords2, axis=1))
