e75afacca10de47d4d6225e69372171d5cc7ae9a
Kyle Johnsen <johnsenkyle13@gmail.com>
2022-02-10 19:50:20 -0500

    finish odds and ends docstrings

--- cleosim/stimulators.py
+++ cleosim/stimulators.py
@@ -1,8 +1,26 @@
+"""Contains basic stimulators."""
+from brian2 import Unit
 from cleosim.base import Stimulator
 
 
 class StateVariableSetter(Stimulator):
-    def __init__(self, name, variable_to_ctrl, unit, start_value=0):
+    """Sets the given state variable of target neuron groups."""
+
+    def __init__(
+        self, name: str, variable_to_ctrl: str, unit: Unit, start_value: float = 0
+    ):
+        """
+        Parameters
+        ----------
+        name : str
+            Unique device name
+        variable_to_ctrl : str
+            Name of state variable to control
+        unit : Unit
+            Unit of that state variable: will be used in :meth:`update`
+        start_value : float, optional
+            Starting variable value, by default 0
+        """
         super().__init__(name, start_value)
         self.neuron_groups = []
         self.var = variable_to_ctrl
@@ -13,6 +31,14 @@ class StateVariableSetter(Stimulator):
         self.neuron_groups.append(neuron_group)
         self.update(self.value)
 
-    def update(self, ctrl_signal):
+    def update(self, ctrl_signal: float) -> None:
+        """Set state variable of target neuron groups
+
+        Parameters
+        ----------
+        ctrl_signal : float
+            Value to update variable to, without unit. The unit
+            provided on initialization is automatically multiplied.
+        """
         for ng in self.neuron_groups:
             setattr(ng, self.var, ctrl_signal * self.unit)
