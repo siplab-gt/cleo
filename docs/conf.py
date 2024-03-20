9b6161c0162d6e1c534f4f4249dafcc755adae0d
Kyle Johnsen <johnsenkyle13@gmail.com>
2024-03-20 16:19:10 -0400

    update copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Cleo"
-copyright = "2023"
+copyright = "2024"
 author = "Kyle Johnsen, Nathan Cruzado"
 
 
